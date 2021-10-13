b4e42adea4ba0a79d560e41950c2e2d32b138bcb
chuvi1155 <chuvi1155@users.noreply.github.com>
2021-10-13 14:30:22 +0300

    Update AnimatedSequence.cs
    Add choose work sequension with FPS or AnimationTime setup

--- Runtime/Components/AnimatedSequence.cs
+++ Runtime/Components/AnimatedSequence.cs
@@ -21,6 +21,8 @@ public class AnimatedSequence : AnimatedBehaviour
 
     public bool PlayRaw = false;
     public string RawFileName;
+    public bool UseFPS = true;
+    public float FPS = 25;
     byte[] rawData = null;
     Texture2D rawTex = null;
     Sprite rawSprite = null;
@@ -49,6 +51,8 @@ public class AnimatedSequence : AnimatedBehaviour
         if (frameAnimation == null || frameAnimation.Length == 0) return;
         if (image != null) image.sprite = frameAnimation[0];
         if (sprite != null) sprite.sprite = frameAnimation[0];
+        if(UseFPS)
+            AnimationTime = frameAnimation.Length / FPS;
     }
 
     public override void ResetInToState()
@@ -115,7 +119,7 @@ public class AnimatedSequence : AnimatedBehaviour
         if ((PlayRaw && rawData == null) || (!PlayRaw && (frameAnimation == null || frameAnimation.Length == 0))) return;
         if (PlayRaw)
         {
-            int num = (int)(_time * (countRawFrame));
+            int num = (int)(curveValue * (countRawFrame));
             num = Mathf.Clamp(num, 0, countRawFrame - 1);
             if (currentRawIndex != num)
             {
@@ -131,7 +135,7 @@ public class AnimatedSequence : AnimatedBehaviour
             }
             return;
         }
-        int n = (int)(_time * (frameAnimation.Length));
+        int n = (int)(curveValue * (frameAnimation.Length));
         n = Mathf.Clamp(n, 0, frameAnimation.Length - 1);
         if (Exists(SequenceTypes.Image) && image != null) image.sprite = frameAnimation[n];
         if (Exists(SequenceTypes.Sprite) && sprite != null) sprite.sprite = frameAnimation[n];
